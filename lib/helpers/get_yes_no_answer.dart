9df1f1567b8265b95a2ebdb346ab77c6b13a73f7
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2024-01-22 19:34:11 -0500

    Se ajusta la forma de hacer el chat con rol user

--- lib/helpers/get_yes_no_answer.dart
+++ lib/helpers/get_yes_no_answer.dart
@@ -1,55 +1,23 @@
-import 'dart:convert';
+
 
 import 'package:dart_openai/dart_openai.dart';
-import 'package:dio/dio.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class GetYesNoAnswer {
-  /*final _dio = Dio();
-  final url = 'https://api.openai.com/v1/chat/completions';
-  final headers = {
-    'Content-Type':
-        'application/json', // ajusta el tipo de contenido según tus necesidades
-    'Authorization':
-        'Bearer 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', // si es necesario enviar un token de autorización
-  };*/
   Future<Message> getAnswer(String text) async {
-    /*final bodyRequest = {
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        {
-          "role": "system",
-          "content":
-              "tu nombre es Branchito, debes saludar amablemente indicando tu nombre"
-        },
-        {"role": "user", "content": text}
-      ]
-    };*/
-
-   /* try {
-      final response = await _dio.post(
-        url,
-        data: jsonEncode(bodyRequest),
-        options: Options(headers: headers),
-      );
-      print(response);
-    } catch (error) {
-      print('Error $error');
-    }*/
-    //final response = await _dio.get('https://yesno.wtf/api');
     final systemMessage = OpenAIChatCompletionChoiceMessageModel(
       content: [
         OpenAIChatCompletionChoiceMessageContentItemModel.text(
-          "return any message you are given as JSON.",
+          "tu nombre es Branchito, debes saludar amablemente indicando tu nombre",
         ),
       ],
-      role: OpenAIChatMessageRole.assistant,
+      role: OpenAIChatMessageRole.system,
     );
 
     final userMessage = OpenAIChatCompletionChoiceMessageModel(
       content: [
         OpenAIChatCompletionChoiceMessageContentItemModel.text(
-          "Hello, I am a chatbot created by OpenAI. How are you today?",
+          text,
         ),
       ],
       role: OpenAIChatMessageRole.user,
@@ -66,11 +34,11 @@ class GetYesNoAnswer {
       //prompt: text,
     );
 
-    print(chatCompletion.choices.first.message);
+    print(chatCompletion.choices.first.message.toString());
 
     return Message(
       //message: response.data['answer'],
-      message: '...',
+      message: chatCompletion.choices.first.message.toString(),
       //urlimage: response.data['image'],
       sender: FromWho.your,
     );
